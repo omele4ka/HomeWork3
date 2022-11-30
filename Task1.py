a2d78e1ed459d56f45eafa9bd42a648b3a34cce2
Omele4ka <omele4ka@inbox.ru>
2022-11-30 14:59:02 +0300

    Task 1 is solved

--- Task1.py
+++ Task1.py
@@ -2,3 +2,21 @@
 # стоящих на нечётной позиции.
 # Пример: [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+import random
+
+list_len = int(input('Введите количество элементов: '))
+my_list = []
+for i in range(1, list_len + 1):
+    my_list.append(random.randint(1, 100))
+print(f'Исходный список элементов: {my_list}')
+
+def sum_odd_elem(list):
+    count = 0
+    for i in range(1, len(list), 2):
+        count += list[i]
+    return count
+print(f'Сумма элементов, стоящих на нечетной позиции равна {sum_odd_elem(my_list)}')
+
+
+
+
